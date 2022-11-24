bec5f621b4387e963854e3d303ecd9b0783e4fc1
Dmitrii Gundorov <gundorov1987@yandex.ru>
2022-11-24 21:25:01 +0300

    Example007

---
+++ Example007/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+//Console.SetCursorPozition(10, 4);
+//Console.WriteLine("+");
+
+int xa = 1, ya = 1,
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
+
+Console.SetCursorPozition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPozition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPozition(xc, yc);
+Console.WriteLine("+");
