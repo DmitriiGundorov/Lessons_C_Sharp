9dac33d5bb02a745ab1f740c71ceab5be95d9efc
Dmitrii Gundorov <gundorov1987@yandex.ru>
2022-11-25 21:01:28 +0300

    Example007_2

--- Example007/Program.cs
+++ Example007/Program.cs
@@ -4,7 +4,7 @@
 
 int xa = 1, ya = 1,
     xb = 1, yb = 30,
-    xc = 80, yc = 30;
+    xc = 40, yc = 30;
 
 Console.SetCursorPozition(xa, ya);
 Console.WriteLine("+");
@@ -14,3 +14,29 @@ Console.WriteLine("+");
 
 Console.SetCursorPozition(xc, yc);
 Console.WriteLine("+");
+
+/* int x = xa, y = xb
+int count=0
+
+while (count<1000);
+{
+    int what = new Random().Next(0, 3); //[0,3] 0, 1, 2
+    if (what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+    if (what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+    if (what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+    Console.SetCursorPozition(x, y);
+    Console.WriteLine("+");
+    count = Count + 1;
+} */
