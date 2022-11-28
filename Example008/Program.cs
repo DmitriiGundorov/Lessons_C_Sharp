a302a4fbc5fbce2bb6bf7d16f2ddb87357d9bf84
Dmitrii Gundorov <gundorov1987@yandex.ru>
2022-11-28 22:37:12 +0300

    Example008_2

--- Example008/Program.cs
+++ Example008/Program.cs
@@ -1,4 +1,5 @@
 ﻿// Максимальное из 9-ти чисел. Стихийный метод
+
 int a1 = 10;
 int b1 = 12;
 int c1 = 15;
@@ -7,9 +8,9 @@ int b2 = 185;
 int c2 = 20;
 int a3 = 22;
 int b3 = 25;
-int c3 = 30;
+int c3 = 3086; 
 
-int max = a1;
+/*int max = a1;
 if (b1 > max) max = b1;
 if (c1 > max) max = c1;
 
@@ -21,4 +22,23 @@ if (a3 > max) max = a3;
 if (b3 > max) max = b3;
 if (c3 > max) max = c3;
 
+Console.WriteLine(max);*/
+
+// Максимальное из 9-ти чисел. Метод функции
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+//int max1 = Max(a1, b1, c1);
+//int max2 = Max(a2, b2, c2);
+//int max3 = Max(a3, b3, c3);
+//int max = Max(max1, max2, max3);
+
+int max =Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
 Console.WriteLine(max);
