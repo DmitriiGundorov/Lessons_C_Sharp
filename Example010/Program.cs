d7eff9cd35630c4dfc7a9b2d9de33f1bf5082d91
Dmitrii Gundorov <gundorov1987@yandex.ru>
2022-11-30 20:04:58 +0300

    Example010

---
+++ Example010/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 1, 3, 5, 8, 9, 12, 45, 8 };
+int n = array.Length;
+int find = 8;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
